5bdb6a4eea946083ce9f3237731b42abd2dd8827
Jonny Barnes <jonny@jonnybarnes.uk>
2016-04-08 03:37:25 +0100

    Hush login details

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,7 @@ BASEDIR=$(pwd)
 
 # ln the various files
 test -L $HOME/.curlrc || ln -f -s $BASEDIR/curlrc $HOME/.curlrc
+test -L $HOME/.hushlogin || ln -f -s $BASEDIR/hushlogin $HOME/.hushlogin
 test -L $HOME/.zshrc || ln -f -s $BASEDIR/zshrc $HOME/.zshrc
 
 # .gitconfig gets edited by .extra so we won’t symlink it, but copy it
