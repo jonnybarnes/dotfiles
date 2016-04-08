4ee2e83109ac0efa7e77a4318294d887dc495cbe
Jonny Barnes <jonny@jonnybarnes.uk>
2016-04-08 01:39:33 +0100

    Simple set up of dotfiles

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+# Current dir
+BASEDIR=$(pwd)
+
+# ln the various files
+test -L $HOME/.curlrc || ln -f -s $BASEDIR/curlrc $HOME/.curlrc
+test -L $HOME/.zshrc || ln -f -s $BASEDIR/zshrc $HOME/.zshrc
+
+# .gitconfig gets edited by .extra so we won’t symlink it, but copy it
+cp $BASEDIR/gitconfig $HOME/.gitconfig
+
+# Source zshrc
+source $HOME/.zshrc
