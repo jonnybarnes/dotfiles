4c376c8b2e66e1204fe77c788e277dcce90c379f
Jonny Barnes <jonny@jonnybarnes.uk>
2016-05-13 08:23:58 +0100

    Add vim stuff

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,6 +9,10 @@ test -L $HOME/.gitignore || ln -f -s $BASEDIR/gitignore $HOME/.gitignore
 test -L $HOME/.hushlogin || ln -f -s $BASEDIR/hushlogin $HOME/.hushlogin
 test -L $HOME/.zshrc || ln -f -s $BASEDIR/zshrc $HOME/.zshrc
 
+# ln vim files
+test -L $HOME/.vim || ln -f -s $BASEDIR/vim $HOME/.vim
+test -L $HOME/.vimrc || ln -f -s $HOME/.vim/vimrc $HOME/.vimrc
+
 # .gitconfig gets edited by .extra so we won’t symlink it, but copy it
 cp $BASEDIR/gitconfig $HOME/.gitconfig
 
