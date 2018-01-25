07450d4ad9d938e29455bf676e2833b961ebfe24
Jonny Barnes <jonny@jonnybarnes.uk>
2018-01-25 12:45:36 +0000

    Load the powerline status on Linux

--- zsh/powerline-prompt.zsh
+++ zsh/powerline-prompt.zsh
@@ -4,3 +4,5 @@ powerline-daemon -q
 
 test $PLATFORM = 'osx' \
 && source /usr/local/lib/python3.6/site-packages/powerline/bindings/zsh/powerline.zsh
+test $PLATFORM = 'linux' \
+&& source /usr/lib/python3.6/site-packages/powerline/bindings/zsh/powerline.zsh
