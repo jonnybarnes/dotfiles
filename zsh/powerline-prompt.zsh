b990effc15b575f2f4e4de32d16a68500e2fe73a
Jonny Barnes <jonny@jonnybarnes.uk>
2018-04-14 18:21:43 +0100

    Some prior work on powerline

--- zsh/powerline-prompt.zsh
+++ zsh/powerline-prompt.zsh
@@ -2,7 +2,9 @@
 
 powerline-daemon -q
 
-test $PLATFORM = 'osx' \
-&& source /usr/local/lib/python3.6/site-packages/powerline/bindings/zsh/powerline.zsh
-test $PLATFORM = 'linux' \
-&& source /usr/lib/python3.6/site-packages/powerline/bindings/zsh/powerline.zsh
+test -e '/usr/local/lib/python3.6/site-packages/powerline/bindings/zsh/powerline.zsh' \
+    && source /usr/local/lib/python3.6/site-packages/powerline/bindings/zsh/powerline.zsh
+test -e '/usr/lib/python3.6/site-packages/poewrline/bindings/zsh/powerline.zsh' \
+    && source /usr/lib/python3.6/site-packages/powerline/bindings/zsh/powerline.zsh
+test -e '/usr/local/lib/python3.5/dist-packages/powerline_status-2.6.dev9999_git.b_a4a1c8353924959188d62c9edcf977cce3aa231f_-py3.5.egg/powerline/bindings/zsh/powerline.zsh' \
+    && source /usr/local/lib/python3.5/dist-packages/powerline_status-2.6.dev9999_git.b_a4a1c8353924959188d62c9edcf977cce3aa231f_-py3.5.egg/powerline/bindings/zsh/powerline.zsh
