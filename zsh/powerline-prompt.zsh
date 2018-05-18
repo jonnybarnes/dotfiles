21506b47a355f2af2d294a4721bee998fcc5a32e
Jonny Barnes <jonny@jonnybarnes.uk>
2018-05-18 16:52:40 +0100

    Fix typo in file path

--- zsh/powerline-prompt.zsh
+++ zsh/powerline-prompt.zsh
@@ -4,7 +4,7 @@ powerline-daemon -q
 
 test -e '/usr/local/lib/python3.6/site-packages/powerline/bindings/zsh/powerline.zsh' \
     && source /usr/local/lib/python3.6/site-packages/powerline/bindings/zsh/powerline.zsh
-test -e '/usr/lib/python3.6/site-packages/poewrline/bindings/zsh/powerline.zsh' \
+test -e '/usr/lib/python3.6/site-packages/powerline/bindings/zsh/powerline.zsh' \
     && source /usr/lib/python3.6/site-packages/powerline/bindings/zsh/powerline.zsh
 test -e '/home/jonny/.local/lib/python3.5/site-packages/powerline/bindings/zsh/powerline.zsh' \
     && source /home/jonny/.local/lib/python3.5/site-packages/powerline/bindings/zsh/powerline.zsh
