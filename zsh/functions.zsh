de7db3ac3a87997e9aab939477c972c469199a43
Jonny Barnes <jonny@jonnybarnes.uk>
2018-06-23 20:28:58 +0100

    Use a consistent location for ca files, relative to home folder anyway

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -6,13 +6,11 @@
 gencert () {
     DOMAIN=$1
 
-    test -d /Users/jonny/git/localCA && ROOT='/Users/jonny/git/localCA'
-    test -d /home/jonny/git/ca && ROOT='/home/jonny/git/ca'
     test -d /usr/local/opt/openssl@1.1/bin && PATH='/usr/local/opt/openssl@1.1/bin':$PATH
     test -f /usr/local/etc/openssl@1.1/openssl.cnf && SSLCNF='/usr/local/etc/openssl@1.1/openssl.cnf'
     test -f /etc/ssl/openssl.cnf && SSLCNF='/etc/ssl/openssl.cnf'
 
-    cd $ROOT
+    cd $HOME/git/ca
     [[ ! -d $DOMAIN ]] && mkdir $DOMAIN
     cd $DOMAIN
     [[ -f key ]] && mv key key.bak
@@ -24,6 +22,6 @@ gencert () {
     openssl req -new -sha256 -key key -subj "/C=UK/ST=England/L=Darwen/O=JMB Dev Ltd/CN=$DOMAIN" -reqexts SAN -config <(cat $SSLCNF <(printf "[SAN]\nsubjectAltName=DNS:$DOMAIN")) -out csr
     openssl x509 -req -in csr -extfile <(cat $SSLCNF <(printf "[SAN]\nsubjectAltName=DNS:$DOMAIN")) -extensions SAN -CA ../jmb-ca-ecc.pem -CAkey ../jmb-ca-ecc.key -CAcreateserial -days 90 -sha256 -out crt
 
-    cd $HOME
+    cd $HOME/git/ca
     echo 'Certs generated for $DOMAIN'
 }
