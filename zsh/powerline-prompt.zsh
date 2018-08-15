697eed6691218fec8ba56342092a74b1288f41bc
Jonny Barnes <jonny@jonnybarnes.uk>
2018-08-15 20:37:48 +0100

    Add check for python3.7 installed powerline

--- zsh/powerline-prompt.zsh
+++ zsh/powerline-prompt.zsh
@@ -4,6 +4,8 @@ powerline-daemon -q
 
 test -e '/usr/local/lib/python3.6/site-packages/powerline/bindings/zsh/powerline.zsh' \
     && source /usr/local/lib/python3.6/site-packages/powerline/bindings/zsh/powerline.zsh
+test -e '/usr/lib/python3.7/site-packages/powerline/bindings/zsh/powerline.zsh' \
+    && source /usr/lib/python3.7/site-packages/powerline/bindings/zsh/powerline.zsh
 test -e '/usr/lib/python3.6/site-packages/powerline/bindings/zsh/powerline.zsh' \
     && source /usr/lib/python3.6/site-packages/powerline/bindings/zsh/powerline.zsh
 test -e '/home/jonny/.local/lib/python3.5/site-packages/powerline/bindings/zsh/powerline.zsh' \
