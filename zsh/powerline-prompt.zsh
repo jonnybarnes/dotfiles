ea002ef1ebe9864c75ac25047ec410387b021ee3
Jonny Barnes <jonny@jonnybarnes.uk>
2019-11-16 09:28:51 +0000

    Add new python/powerline binding source statement

--- zsh/powerline-prompt.zsh
+++ zsh/powerline-prompt.zsh
@@ -10,3 +10,5 @@ test -e '/usr/lib/python3.6/site-packages/powerline/bindings/zsh/powerline.zsh' 
     && source /usr/lib/python3.6/site-packages/powerline/bindings/zsh/powerline.zsh
 test -e '/home/jonny/.local/lib/python3.5/site-packages/powerline/bindings/zsh/powerline.zsh' \
     && source /home/jonny/.local/lib/python3.5/site-packages/powerline/bindings/zsh/powerline.zsh
+test -e '/usr/lib/python3.8/site-packages/powerline/bindings/zsh/powerline.zsh' \
+    && source /usr/lib/python3.8/site-packages/powerline/bindings/zsh/powerline.zsh
