971b9730d4804d543a840405b39c277283b37550
Jonny Barnes <jonny@jonnybarnes.uk>
2021-07-17 18:24:07 +0100

    Add M1 platform support

--- zsh/platform.zsh
+++ zsh/platform.zsh
@@ -1,7 +1,12 @@
 export PLATFORM="unkown"
-unamestr=$(uname -s)
-if [[ "$unamestr" == 'Linux' ]]; then
+osname=$(uname -s)
+cputpye=£(uname -m)
+if [[ "$osname" == 'Linux' ]]; then
     export PLATFORM="linux"
-elif [[ "$unamestr" == 'Darwin' ]]; then
-    export PLATFORM="osx"
+elif [[ "$osname" == 'Darwin' ]]; then
+    if [[ "$cputpye" == 'x86_64' ]]; then
+        export PLATFORM="osx"
+    elif [[ "$cputype" == 'arm64' ]]; then
+        export PLATFORM="osx-m1"
+    fi
 fi
