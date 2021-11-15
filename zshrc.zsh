3883e99891791642fceff0ef3c1a7b1be7289588
Jonny Barnes <jonny@jonnybarnes.uk>
2021-11-15 19:50:45 +0000

    Add fig setup

--- zshrc.zsh
+++ zshrc.zsh
@@ -1,3 +1,8 @@
+#### FIG ENV VARIABLES ####
+# Please make sure this block is at the start of this file.
+[ -s ~/.fig/shell/pre.sh ] && source ~/.fig/shell/pre.sh
+#### END FIG ENV VARIABLES ####
+
 # User configuration
 # history
 HISTFILE=~/.histfile
