a54a55ba1de562b2c87891e524de15b1f94a0954
Jonny Barnes <jonny@jonnybarnes.uk>
2023-06-04 18:49:01 +0100

    Use vivid for LS_COLORS

--- zshrc.zsh
+++ zshrc.zsh
@@ -131,6 +131,9 @@ export CLICOLOR=true
 export CLICOLOR_FORCE=true
 export LSCOLORS='dxfxcxdxbxegedabagacad'
 export LS_COLORS='di=33;40:ln=35;40:so=32;40:pi=33;40:ex=31;40:bd=34;46:cd=34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43:'
+if type vivid &>/dev/null; then
+  export LS_COLORS="$(vivid generate ayu)"
+fi
 
 # Source my own functions
 test -e $HOME/.zsh/functions.zsh && source $HOME/.zsh/functions.zsh
