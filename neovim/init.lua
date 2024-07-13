a2b2db8757e70a4daf7c19a08d88a2b8095c2833
Jonny Barnes <jonny@jonnybarnes.uk>
2024-07-13 17:04:18 +0100

    Add line numbers in NeoVim

--- neovim/init.lua
+++ neovim/init.lua
@@ -6,5 +6,8 @@ require('nvim-web-devicons').setup()
 require('oil').setup()
 require('gitsigns').setup()
 
+-- Editor options
+vim.wo.number = true
+
 vim.opt.spelllang = 'en_gb'
 vim.opt.spell = true
