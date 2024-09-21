bcd3112ec95a80830cd28f8532912cb782286061
Jonny Barnes <jonny@jonnybarnes.uk>
2024-09-21 09:02:04 +0100

    Add phpactor support for neovim

--- neovim/init.lua
+++ neovim/init.lua
@@ -5,6 +5,7 @@ My NeoVim configuration
 require('nvim-web-devicons').setup()
 require('oil').setup()
 require('gitsigns').setup()
+require('lspconfig').phpactor.setup({})
 
 -- Editor options
 vim.wo.number = true
