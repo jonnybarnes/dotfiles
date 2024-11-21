00898fae4c2f7aab0ad5fe2b95da8c4bee0dd2c8
Jonny Barnes <jonny@jonnybarnes.uk>
2024-11-21 19:54:54 +0000

    Updated neovim config

--- neovim/init.lua
+++ neovim/init.lua
@@ -2,8 +2,14 @@
 My NeoVim configuration
 --]]
 
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- Plugins
 require('nvim-web-devicons').setup()
-require('oil').setup()
+-- require('oil').setup()
+require('nvim-tree').setup()
 require('gitsigns').setup()
 require('lspconfig').phpactor.setup({})
 
@@ -12,3 +18,6 @@ vim.wo.number = true
 
 vim.opt.spelllang = 'en_gb'
 vim.opt.spell = true
+
+-- 24-bit colour support
+vim.opt.termguicolors = true
