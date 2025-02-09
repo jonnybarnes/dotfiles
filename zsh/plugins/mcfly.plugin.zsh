312fb07560d51e40ece2ff83e96f32fddcee4864
Jonny Barnes <jonny@jonnybarnes.uk>
2025-02-09 21:35:43 +0000

    Load some zsh plugins directly from github

--- zsh/plugins/mcfly.plugin.zsh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-
-# McFly
-if (( ${+commands[mcfly]} )); then
-  eval "$(mcfly init zsh)"
-fi
--- zsh/plugins/zoxide.plugin.zsh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-
-# zoxide - a better `cd` command
-if (( ${+commands[zoxide]} )); then
-  eval "$(zoxide init zsh)"
-fi
