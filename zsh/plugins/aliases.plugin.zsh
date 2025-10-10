fe2213fe9126fa348e338be49a82f873b15b41b9
Jonny Barnes <jonny@jonnybarnes.uk>
2025-10-10 13:15:08 +0100

    Add yay+fzf alias

--- zsh/plugins/aliases.plugin.zsh
+++ zsh/plugins/aliases.plugin.zsh
@@ -12,3 +12,6 @@ alias sail="[ -f sail ] && bash sail || bash vendor/bin/sail"
 
 # Add git aliases
 alias gs="git status"
+
+# Add yay+fzf alias
+alias yayf="yay -Slq | fzf --multi --preview 'yay -Sii {1}' --preview-window=down:75% --layout=reverse | xargs -ro yay -S"
